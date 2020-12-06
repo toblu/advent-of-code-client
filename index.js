0b01fe319f3621ccee55f26fe660a25d1c2b0fa9
Tobias Lundell <tobias.lundell@ericsson.com>
2020-12-06 22:50:59 +0100

    add some pre-defined transform functions

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const AocClient = require('./src/AocClient');
+const transforms = require('./src/util/transforms');
 
-module.exports = AocClient;
+module.exports = {
+  AocClient,
+  transforms
+};
